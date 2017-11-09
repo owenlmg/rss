98e5c7eadee869c56bb06d29b141cb7a160a1c7d
owenlmg <owenlmg@gmail.com>
2017-11-09 09:24:41 +0800

    增加feed rss，删除vehicle

--- RSS/src/main/java/com/lmg/springboot/App.java
+++ RSS/src/main/java/com/lmg/springboot/App.java
@@ -4,15 +4,13 @@
  */
 package com.lmg.springboot;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lmg.springboot.feed.service.FeedService;
 import com.lmg.springboot.listeners.MyApplicationListener;
-import com.lmg.springboot.vehicle.repositories.VehicleRepository;
 
 /***********************************
  * @ClassName: StartController.java
@@ -25,10 +23,7 @@ import com.lmg.springboot.vehicle.repositories.VehicleRepository;
 public class App implements CommandLineRunner {
     
     @Autowired
-    DataSource        dataSource;
-    
-    @Autowired
-    VehicleRepository vehicleRepository;
+    FeedService feedService;
     
     
     public static void main(String[] args) {
@@ -47,8 +42,11 @@ public class App implements CommandLineRunner {
      * @createaAt:2017年11月7日上午10:50:35
      */
     public void run(String... args) throws Exception {
-        // System.out.println(dataSource);
-        // System.out.println(vehicleRepository.findOne(828));
+        String link = "http://www.36kr.com/feed";
+        
+        System.out.println(feedService.addFeed(link));
+        System.out.println(feedService.findByLink(link));
+        // 定时任务
     }
     
 }
